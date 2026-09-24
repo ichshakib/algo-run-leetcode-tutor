45ba9b48a39eb97374cc261e5175f3fa33b06c5e
Shakib Khan <ichshakib@gmail.com>
2026-09-24 07:23:08 +0600

    chore: install dependencies and setup base configuration for Chrome extension

--- chrome-extension/manifest.config.ts
+++ chrome-extension/manifest.config.ts
@@ -1,29 +1,31 @@
-import { defineManifest } from '@crxjs/vite-plugin'
-import pkg from './package.json'
+import { defineManifest } from "@crxjs/vite-plugin"
+import pkg from "./package.json" with { type: "json" }
 
 export default defineManifest({
   manifest_version: 3,
-  name: pkg.name,
-  description: pkg.description,
+  name: pkg.extension_name,
+  description: pkg.extension_description,
   version: pkg.version,
   icons: {
-    48: 'public/logo.png',
+    "16": "public/icons/icon16.png",
+    "32": "public/icons/icon32.png",
+    "48": "public/icons/icon48.png",
+    "128": "public/icons/icon128.png",
   },
-  action: {
-    default_icon: {
-      48: 'public/logo.png',
-    },
-    default_popup: 'src/popup/index.html',
-  },
-  permissions: [
-    'sidePanel',
-    'contentSettings',
-  ],
-  content_scripts: [{
-    js: ['src/content/main.tsx'],
-    matches: ['https://*/*'],
-  }],
   side_panel: {
-    default_path: 'src/sidepanel/index.html',
+    default_path: "src/sidepanel/index.html",
   },
+  web_accessible_resources: [
+    { resources: ["logos/logo.svg"], matches: ["<all_urls>"] },
+  ],
+  permissions: ["storage", "sidePanel"],
+  action: {
+    default_popup: "src/popup/index.html",
+  },
+  content_scripts: [
+    {
+      js: ["src/content/main.tsx"],
+      matches: ["https://leetcode.com/problems/*"],
+    },
+  ],
 })
